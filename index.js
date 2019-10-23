d9103a993d7ce7787ac5fc7cca20457f2ce02b3c
wuyum <397642351@qq.com>
2019-10-23 19:30:28 +0800

    0.1.0

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-module.exports = require('./eslintrc.json')
+const globals = require("globals");
+const rules = require("./rules");
+module.exports = {
+  "parserOptions": {
+    "ecmaVersion": 2020,
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "sourceType": "module"
+  },
+
+  "env": {
+    "node": true,
+    "browser": true
+  },
+
+  "plugins": [
+    "import",
+    "node",
+    "promise",
+    "standard"
+  ],
+
+  "globals": {
+    "document": "readonly",
+    "navigator": "readonly",
+    "window": "readonly",
+    ...globals.es2015
+  },
+
+  "rules": rules
+}
+
